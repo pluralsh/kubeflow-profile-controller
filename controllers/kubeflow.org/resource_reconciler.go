ef7e43acb86e7c5598e97e71176b726c8679c8ca
DavidSpek <vanderspek.david@gmail.com>
2021-08-09 13:44:24 +0200

    Init move

---
+++ controllers/kubeflow.org/resource_reconciler.go
@@ -0,0 +1,96 @@
+package controllers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"text/template"
+
+	profilev2alpha1 "github.com/pluralsh/kubeflow-profile-controller/apis/kubeflow.org/v2alpha1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	kubeyaml "k8s.io/apimachinery/pkg/util/yaml"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	"sigs.k8s.io/yaml"
+)
+
+func deserializeExtraResources(profile *profilev2alpha1.Profile) ([]*unstructured.Unstructured, error) {
+	var out bytes.Buffer
+	for ind, extra := range profile.Spec.ExtraResources {
+		tmpl, err := template.ParseFiles(extra.FileName)
+		if err != nil {
+			return nil, err
+		}
+
+		if ind > 0 {
+			out.Write([]byte("---\n"))
+		}
+
+		varsJSON, err := extra.Vars.MarshalJSON()
+		if err != nil {
+			return nil, err
+		}
+
+		var tempVars interface{}
+		if varErr := json.Unmarshal(varsJSON, &tempVars); varErr != nil {
+			return nil, varErr
+		}
+
+		if err := tmpl.Execute(&out, tempVars); err != nil {
+			return nil, err
+		}
+	}
+	return splitYaml(out.Bytes())
+}
+
+func (r *ProfileReconciler) reconcileExtraResources(ctx context.Context, profile *profilev2alpha1.Profile) error {
+	logger := r.Log.WithValues("profile", profile.Name)
+	objs, err := deserializeExtraResources(profile)
+	if err != nil {
+		return err
+	}
+
+	for _, obj := range objs {
+		obj.SetNamespace(profile.Name)
+
+		if err := controllerutil.SetControllerReference(profile, obj, r.Scheme); err != nil {
+			return err
+		}
+
+		logger.Info("Applying extra resource", "kind", obj.GetKind(), "name", obj.GetName())
+		if err := r.Patch(ctx, obj, client.Apply, client.ForceOwnership, client.FieldOwner("profile-controller")); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func splitYaml(yamlData []byte) ([]*unstructured.Unstructured, error) {
+	d := kubeyaml.NewYAMLOrJSONDecoder(bytes.NewReader(yamlData), 4096)
+	var objs []*unstructured.Unstructured
+	for {
+		ext := runtime.RawExtension{}
+		if err := d.Decode(&ext); err != nil {
+			if err == io.EOF {
+				break
+			}
+			return objs, fmt.Errorf("failed to unmarshal manifest: %v", err)
+		}
+		ext.Raw = bytes.TrimSpace(ext.Raw)
+		if len(ext.Raw) == 0 || bytes.Equal(ext.Raw, []byte("null")) {
+			continue
+		}
+		u := &unstructured.Unstructured{}
+		if err := yaml.Unmarshal(ext.Raw, u); err != nil {
+			return objs, fmt.Errorf("failed to unmarshal manifest: %v", err)
+		}
+		objs = append(objs, u)
+	}
+	return objs, nil
+}
---
+++ go.mod
@@ -0,0 +1,28 @@
+module github.com/pluralsh/kubeflow-profile-controller
+
+go 1.16
+
+require (
+	github.com/aws/aws-sdk-go v1.40.1
+	github.com/cenkalti/backoff v2.2.1+incompatible
+	github.com/fsnotify/fsnotify v1.4.9
+	// github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-logr/logr v0.4.0
+	github.com/goccy/go-yaml v1.8.10
+	github.com/onsi/ginkgo v1.16.4
+	github.com/onsi/gomega v1.14.0
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.11.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.0
+	github.com/tidwall/gjson v1.8.1
+	golang.org/x/oauth2 v0.0.0-20210628180205-a41e5a781914
+	google.golang.org/api v0.50.0
+	istio.io/api v0.0.0-20210713195055-3a340e4f154e
+	istio.io/client-go v1.10.3
+	k8s.io/api v0.21.3
+	k8s.io/apimachinery v0.21.3
+	k8s.io/client-go v0.21.3
+	sigs.k8s.io/controller-runtime v0.9.2
+	sigs.k8s.io/yaml v1.2.0
+)
